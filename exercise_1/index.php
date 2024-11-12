8553cf327da4c0d06934bf6240c90a7ae7baa4e3
banh02 <ngoanhn06@gmail.com>
2024-11-12 10:33:56 +0700

    index.php

--- exercise_1/index.php
+++ exercise_1/index.php
@@ -5,39 +5,7 @@
     <meta charset="UTF-8">
     <title>Máy Tính Chiết Khấu Sản Phẩm</title>
 </head>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
 
-    html {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        /* text-align: end; */
-        color: #fff;
-        background: #fff;
-    }
-
-    body {
-        background: #333;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 500px;
-        height: 300px;
-    }
-
-    button {
-        border-radius: 60px;
-        padding: 10px 10px;
-        background-color: blueviolet;
-        cursor: pointer;
-    }
-</style>
 
 <body>
     <h2>Máy Tính Chiết Khấu Sản Phẩm</h2>
